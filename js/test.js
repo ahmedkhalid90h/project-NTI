b99672d7bc79e2b4392526cf3a0ac971979629e6
ahmedkhalid90h <ahmedkhalid90h@gmail.com>
2023-03-25 13:50:30 +0200

    project NTI

---
+++ js/test.js
@@ -0,0 +1,71 @@
+
+
+
+// // <!--  task ahmed khalid -->
+
+
+// // $(document).ready(function(){
+// //     $(".owl-carousel").owlCarousel();
+// //     items : 2,
+// // }
+// // );
+
+
+
+
+
+// // var array = []
+// // function userName() {
+// //     var infoUser = {
+// //         nam : prompt("enter your frist name 1"),
+// //         id : prompt("enter your frist id 1"),
+// //         email : prompt("enter your email 1")
+// //     }
+    
+// //     array.push(infoUser)
+// // }
+
+// console.log(array);
+// for ( i = 1; i <= 3 ;  i++) {
+//     userName()
+// }
+
+// function editUser() {
+//     id = prompt("enter your id to edit")
+//     newEmail = prompt("enter your new mail ")
+//     var right = array.findIndex(item => item.id == id)
+//     array[right].email = newEmail
+// }
+// editUser()
+
+// function deletUser(){
+//     id = prompt("enter your delet user")
+//     var right = array.findIndex(item => item.id == id)
+//     array.splice(right,1)
+// }
+
+// deletUser()
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
